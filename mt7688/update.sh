070b1050b391ee3bb4f12c51762f8405cec5b73b
Luong Tran <72029180+TGLuong@users.noreply.github.com>
2024-07-10 15:47:03 +0700

    Update update.sh

--- mt7688/update.sh
+++ mt7688/update.sh
@@ -1,3 +1,8 @@
+/etc/init.d/relay-agent disable
+/etc/init.d/hc-module disable
+/etc/init.d/hc-module stop
+/etc/init.d/relay-agent stop
+
 # ====================== hc-module ======================
 HC_MODULE_OTA_CHECKSUM=`curl -s https://raw.githubusercontent.com/2d6c067f482c6202978fe59a0927e10a/90af189f829b5a981de15e22bb9e8efb/master/mt7688/hc-module/checksum`
 HC_MODULE_CURRENT_CHECKSUM=`sha256sum /hc-module/hc-module | awk '{print $1}'`
