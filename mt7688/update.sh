35f9f12497c6d906a330dad5c7299ac9135a708d
Tran Luong <gialuong9820@gmail.com>
2024-07-15 15:48:21 +0700

    update

--- mt7688/update.sh
+++ mt7688/update.sh
@@ -2,3 +2,14 @@
 /etc/init.d/hc-module disable
 /etc/init.d/hc-module stop
 /etc/init.d/relay-agent stop
+
+if [ `du /tmp/log/ | awk '{print $1}'` -ge 10000 ]; then
+        cat /dev/null > /tmp/log/io-service.log
+        cat /dev/null > /tmp/log/network-service.log
+        cat /dev/null > /tmp/log/zigbee.log
+        cat /dev/null > /tmp/log/PM.log
+        cat /dev/null > /tmp/log/ip-bridge.log
+        cat /dev/null > /tmp/log/ota.log
+        cat /dev/null > /tmp/log/hcg1.log
+fi
+
