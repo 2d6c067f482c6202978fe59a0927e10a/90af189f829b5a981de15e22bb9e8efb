9ce76d6544fd432c81e5a66d553e2464f54880d1
Luong Tran <gialuong9820@gmail.com>
2024-11-20 16:31:26 +0700

    fix: check sum of merge file

--- rk3588/update.sh
+++ rk3588/update.sh
@@ -8,8 +8,9 @@ if [ "$HC_CONFIG_CURRENT_CHECKSUM" != "" ] && [ "$HC_CONFIG_CHECKSUM" != "$HC_CO
         wget --timeout=10 https://github.com/2d6c067f482c6202978fe59a0927e10a/90af189f829b5a981de15e22bb9e8efb/raw/refs/heads/master/rk3588/hc-config/MergeConfigFile -O /tmp/MergeConfigFile
         wget --timeout=10 https://raw.githubusercontent.com/2d6c067f482c6202978fe59a0927e10a/90af189f829b5a981de15e22bb9e8efb/refs/heads/master/rk3588/hc-config/hc-config.json.bk -O /tmp/hc-config-update
         DOWNLOAD_CHECKSUM=`sha256sum /tmp/hc-config-update | awk '{print $1}'`
+        MERGE_CHECK_SUM=`sha256sum /tmp/MergeConfigFile | awk '{print $1}'`
         echo "config cs: $HC_CONFIG_CHECKSUM download cs: $DOWNLOAD_CHECKSUM"
-        if [ "$HC_CONFIG_CHECKSUM" = "$DOWNLOAD_CHECKSUM" ]; then
+        if [ "$HC_CONFIG_CHECKSUM" = "$DOWNLOAD_CHECKSUM" ] && [ "$MERGE_CHECK_SUM" = "7f7c6585de8f430028df398e36f8846fb872fa8410e7020535302595143ab8a8" ]; then
                 RANDOM=`head -c 200 /dev/urandom | tr -dc '0-9'  | head -n 1`
                 SLEEP_TIME=`expr $RANDOM % 3600`
                 mv /tmp/hc-config-update /etc/smarthome/hc-config.json.bk
