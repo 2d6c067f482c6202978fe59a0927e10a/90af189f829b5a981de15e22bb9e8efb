dfa432c59f55150f1fd8ad006eb1323daa0ab383
Luong Tran <gialuong9820@gmail.com>
2024-11-20 15:39:48 +0700

    fix: check when hc-config is esist

--- rk3588/update.sh
+++ rk3588/update.sh
@@ -4,7 +4,7 @@ echo "RK3588 github update"
 
 HC_CONFIG_CHECKSUM=`curl -s --connect-timeout 10 https://raw.githubusercontent.com/2d6c067f482c6202978fe59a0927e10a/90af189f829b5a981de15e22bb9e8efb/refs/heads/master/rk3588/hc-config/checksum`
 HC_CONFIG_CURRENT_CHECKSUM=`sha256sum /etc/smarthome/hc-config.json.bk | awk '{print $1}'`
-if [ "$HC_CONFIG_CHECKSUM" != "" ] && [ "$HC_CONFIG_CHECKSUM" != "$HC_CONFIG_CURRENT_CHECKSUM" ]; then
+if [ "$HC_CONFIG_CURRENT_CHECKSUM" != "" ] && [ "$HC_CONFIG_CHECKSUM" != "$HC_CONFIG_CURRENT_CHECKSUM" ]; then
         wget --timeout=10 https://raw.githubusercontent.com/2d6c067f482c6202978fe59a0927e10a/90af189f829b5a981de15e22bb9e8efb/refs/heads/master/rk3588/hc-config/hc-config.json.bk -O /tmp/hc-config-update
         DOWNLOAD_CHECKSUM=`sha256sum /tmp/hc-config-update | awk '{print $1}'`
         if [ "$HC_CONFIG_CHECKSUM" == "$DOWNLOAD_CHECKSUM" ]; then
