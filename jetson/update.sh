8308ee2f386157ff41e2c412aafd8db41acce5db
Luong Tran <gialuong9820@gmail.com>
2024-12-09 16:53:01 +0700

    fix: check cond

--- jetson/update.sh
+++ jetson/update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-echo 15 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio15/direction
-echo 1 > /sys/class/gpio/gpio15/value
+if [ ! -e "/sys/class/gpio/gpio15/value" ]; then
+    echo "set fan high"
+    echo 15 > /sys/class/gpio/export
+    echo out > /sys/class/gpio/gpio15/direction
+    echo 1 > /sys/class/gpio/gpio15/value
+fi
