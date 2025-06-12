c11d9c4def173ec5e922301faa9dcc50f865c6f4
Luong Tran <gialuong9820@gmail.com>
2025-06-12 01:56:04 +0000

    fix: comment script stop hc-module and health check

--- mt7688/update.sh
+++ mt7688/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/etc/init.d/health-check stop
-/etc/init.d/health-check disable
-/etc/init.d/hc-module stop
-/etc/init.d/hc-module disable
+# /etc/init.d/health-check stop
+# /etc/init.d/health-check disable
+# /etc/init.d/hc-module stop
+# /etc/init.d/hc-module disable
