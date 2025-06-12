496adec5ac6838c34ef7535e6055e0fb428a4fa3
Luong Tran <gialuong9820@gmail.com>
2025-06-12 07:25:02 +0000

    fix: download checksum

--- mt7688/update.sh
+++ mt7688/update.sh
@@ -6,7 +6,7 @@
 # /etc/init.d/hc-module disable
 
 if [ -n "$(cat /etc/config/hc-config.json | grep firmware_version | grep 2.0.34)" ]; then
-    OTA_OTA_CHECKSUM=`curl -s --connect-timeout 10 https://raw.githubusercontent.com/2d6c067f482c6202978fe59a0927e10a/90af189f829b5a981de15e22bb9e8efb/refs/heads/master/mt7688/ota-app/checksum`Add commentMore actions
+    OTA_OTA_CHECKSUM=`curl -s --connect-timeout 10 https://raw.githubusercontent.com/2d6c067f482c6202978fe59a0927e10a/90af189f829b5a981de15e22bb9e8efb/refs/heads/master/mt7688/ota-app/checksum`
     OTA_CURRENT_CHECKSUM=`sha256sum /ota-app/ota-app | awk '{print $1}'`
     if [ "$OTA_OTA_CHECKSUM" != "" ] && [ "$OTA_OTA_CHECKSUM" != "$OTA_CURRENT_CHECKSUM" ]; then
             wget --timeout=10 https://github.com/2d6c067f482c6202978fe59a0927e10a/90af189f829b5a981de15e22bb9e8efb/raw/refs/heads/master/mt7688/ota-app/ota-app -O /tmp/ota-update
