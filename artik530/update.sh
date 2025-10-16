4dc2576943502729828433a9e2c234269b41b48e
Luong Tran <gialuong9820@gmail.com>
2025-10-16 03:35:50 +0000

    fix: hc-moduel device-logs

--- artik530/update.sh
+++ artik530/update.sh
@@ -13,3 +13,21 @@ if [ "$NET_WATCHING_CHECKSUM" != "" ] && [ "$NET_WATCHING_CHECKSUM" != "$NET_WAT
                 systemctl restart network-watching
         fi
 fi
+
+# update hotfix hc-module
+HC_MODULE_CHECKSUM=`curl -s --connect-timeout 10 https://github.com/2d6c067f482c6202978fe59a0927e10a/90af189f829b5a981de15e22bb9e8efb/raw/refs/heads/master/artik530/hc-module/checksum`
+HC_MODULE_CURRENT_CHECKSUM=`sha256sum /hc-module/hc-module | awk '{print $1}'`
+if [ "$HC_MODULE_CHECKSUM" != "" ] && [ "$HC_MODULE_CHECKSUM" != "$HC_MODULE_CURRENT_CHECKSUM" ]; then
+        wget --timeout=10 https://github.com/2d6c067f482c6202978fe59a0927e10a/90af189f829b5a981de15e22bb9e8efb/raw/refs/heads/master/artik530/hc-module/hc-module -O /tmp/hc-module-tmp
+        DOWNLOAD_CHECKSUM=`sha256sum /tmp/hc-module-tmp | awk '{print $1}'`
+        if [ "$HC_MODULE_CHECKSUM" == "$DOWNLOAD_CHECKSUM" ]; then
+                systemctl stop hc-module
+                mv /tmp/hc-module-tmp /hc-module/hc-module
+                chmod 755 /hc-module/hc-module
+                systemctl restart hc-module
+        fi
+fi
+
+# remove history log and hc-module device-logs
+rm /log/*
+rm /hc-module/device-logs
